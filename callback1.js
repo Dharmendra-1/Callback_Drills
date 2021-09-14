5db74070cc3f1909f5d72b6d8457233aeec848a9
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-14 13:59:41 +0530

    Code updated remove lists data and prints only board id and boards data.

--- callback1.js
+++ callback1.js
@@ -2,16 +2,20 @@
 	Problem 1: Write a function that will return a particular board's information based on the boardID that is passed from the given list of boards in boards.json and then pass control back to the code that called it by using a callback function.
 */
 
-const boardsInfo = (board, lists, cb) => {
-  if (board.length == 0 || Object.keys(lists).length == 0) {
-    throw new Error('Boards or lists data is Empty..');
+const boardsInfo = (board, cb) => {
+  if (board.length == 0) {
+    throw new Error('Boards data is Empty..');
   }
 
   setTimeout(() => {
     let idOfBoard = board.map((obj) => obj.id);
     setTimeout(() => {
       idOfBoard.forEach((id) => {
-        return cb(lists[id], id);
+        board.forEach((boardInfo) => {
+          if (boardInfo.id === id) {
+            return cb(boardInfo, id);
+          }
+        });
       });
     }, 2000);
   }, 2000);
