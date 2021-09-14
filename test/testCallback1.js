5934df315da437b1106681bbd5c9f669e0f9009d
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-14 12:27:09 +0530

    import boardsInfo function and boards and lists json data. and calling boardsInfo function with boards, lists argument.

---
+++ test/testCallback1.js
@@ -0,0 +1,5 @@
+const boardsInfo = require('../callback1');
+const boards = require('../data/boards.json');
+const lists = require('../data/lists.json');
+
+const result = boardsInfo(boards, lists);
