2187cff892ea95ad0e7510c523d62b4d77e7b35b
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-14 13:40:05 +0530

    Added try and catch for handling exception and error..

--- test/testCallback1.js
+++ test/testCallback1.js
@@ -2,4 +2,11 @@ const boardsInfo = require('../callback1');
 const boards = require('../data/boards.json');
 const lists = require('../data/lists.json');
 
-const result = boardsInfo(boards, lists);
+try {
+  const result = boardsInfo(boards, lists, (data, id) => {
+    console.log(id);
+    console.log(data);
+  });
+} catch (error) {
+  console.log(error.message);
+}
