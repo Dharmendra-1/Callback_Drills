8028c0398b20dfb6286065657a1621bbb0c5b257
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-14 16:21:38 +0530

    import infoOfThanosBoard , boards, lists, cards data and calling function and used error handling by try catch block.

---
+++ test/testCallback4.js
@@ -0,0 +1,10 @@
+const infoOfThanosBoard = require("../callback4");
+const boards = require("../data/boards.json");
+const lists = require("../data/lists.json");
+const cards = require("../data/cards.json");
+
+try {
+  const result = infoOfThanosBoard(boards, lists, cards);
+} catch (error) {
+  console.log(error.message);
+}
