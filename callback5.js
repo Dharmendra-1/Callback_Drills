236644f288ebd5f7d2dfe147a4d9d597f14be9e6
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 07:59:21 +0530

    unwanted Code removed.

--- callback5.js
+++ callback5.js
@@ -1,6 +1,5 @@
 /* 
-	Problem 5: Write a function that will use the previously written functions to get the following information. You do not need to pass control back to the code that called it.
-
+	Problem 5: 
     Get information from the Thanos boards
     Get all the lists for the Thanos board
     Get all cards for the Mind and Space lists simultaneously
