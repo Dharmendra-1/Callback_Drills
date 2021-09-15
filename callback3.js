64afe8f38424bfe368ae17fab236d5a972abebf5
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 19:58:24 +0530

    create boardId, listOfId, cardsInfo function all return promise , cardsInfo promise resolve info of list id.

--- callback3.js
+++ callback3.js
@@ -1,27 +1,39 @@
-const cardsInfo = (board, lists, cards, cb) => {
-  if (
-    board.length === 0 ||
-    Object.keys(lists).length === 0 ||
-    Object.keys(cards).length === 0
-  ) {
-    throw new Error('Boards or lists or cards is Empty..');
-  }
-
-  setTimeout(() => {
-    const boardId = board.map((obj) => obj.id);
+const boardId = (board) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      boardId.forEach((id) => {
-        let listOfId = lists[id].map((obj) => obj.id);
-        setTimeout(() => {
-          listOfId.forEach((id) => {
-            if (cards.hasOwnProperty(id)) {
-              return cb(cards[id], id);
-            }
-          });
-        }, 2000);
-      });
+      if (board.length == 0) {
+        reject('Boards data is Empty..');
+      } else {
+        let idOfBoard = board.map((obj) => obj.id);
+        resolve(idOfBoard);
+      }
     }, 2000);
-  }, 2000);
+  });
 };
 
-module.exports = cardsInfo;
+const listOfId = (list, id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Object.keys(list).length === 0) {
+        reject('list is empty..');
+      } else {
+        let idOflist = list[id].map((obj) => obj.id);
+        resolve(idOflist);
+      }
+    }, 2000);
+  });
+};
+
+const cardsInfo = (cards, id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Object.keys(cards).length === 0) {
+        reject('cards is Empty..');
+      } else {
+        resolve(cards[id]);
+      }
+    }, 2000);
+  });
+};
+
+module.exports = { boardId, listOfId, cardsInfo };
