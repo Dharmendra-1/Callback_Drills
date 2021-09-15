db20fee0025b999ed523c25c4071a657896f0ce2
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 16:10:32 +0530

    return new Promise which resolve boards info of particular board id.

--- callback1.js
+++ callback1.js
@@ -1,20 +1,20 @@
-const boardsInfo = (board, cb) => {
-  if (board.length == 0) {
-    throw new Error('Boards data is Empty..');
-  }
+const boardsInfo = (board) => {
+  return new Promise((resolve, reject) => {
+    if (board.length == 0) {
+      throw new Error('Boards data is Empty..');
+    }
 
-  setTimeout(() => {
-    let idOfBoard = board.map((obj) => obj.id);
     setTimeout(() => {
+      let idOfBoard = board.map((obj) => obj.id);
       idOfBoard.forEach((id) => {
         board.forEach((boardInfo) => {
           if (boardInfo.id === id) {
-            return cb(boardInfo, id);
+            resolve(boardInfo);
           }
         });
       });
     }, 2000);
-  }, 2000);
+  });
 };
 
 module.exports = boardsInfo;
