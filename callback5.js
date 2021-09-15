62ddec6b279c9da5a8a4df7cbcc57f7787e971d4
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 07:48:28 +0530

    if (     board.length === 0 ||     Object.keys(lists).length === 0 ||  Error Handling Added When Data is not correct.

--- callback5.js
+++ callback5.js
@@ -11,6 +11,14 @@ const listInfo = require('./callback2');
 const cardsInfo = require('./callback3');
 
 const MindAndSpaceCards = (boards, lists, cards) => {
+  if (
+    board.length === 0 ||
+    Object.keys(lists).length === 0 ||
+    Object.keys(cards).length === 0
+  ) {
+    throw new Error('Boards or lists or cards is Empty..');
+  }
+
   setTimeout(() => {
     boardInfo(boards, (data) => {
       if (data.name === 'Thanos') {
