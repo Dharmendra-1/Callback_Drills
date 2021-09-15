7685eefbfa3140c202aa46f4a8e164b961cfeca7
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 07:43:05 +0530

    create MindAndSpaceCards and import previous function and get Mind And Space list data simultaneously.

---
+++ callback5.js
@@ -0,0 +1,48 @@
+/* 
+	Problem 5: Write a function that will use the previously written functions to get the following information. You do not need to pass control back to the code that called it.
+
+    Get information from the Thanos boards
+    Get all the lists for the Thanos board
+    Get all cards for the Mind and Space lists simultaneously
+*/
+
+const boardInfo = require('./callback1');
+const listInfo = require('./callback2');
+const cardsInfo = require('./callback3');
+
+const MindAndSpaceCards = (boards, lists, cards) => {
+  setTimeout(() => {
+    boardInfo(boards, (data) => {
+      if (data.name === 'Thanos') {
+        console.log(data);
+        let thanosId = data.id;
+
+        setTimeout(() => {
+          listInfo(boards, lists, (listData, id) => {
+            if (thanosId == id) {
+              console.log(listData);
+              let idOfList = [];
+              listData.forEach((obj) => {
+                if (obj.name === 'Mind' || obj.name === 'Space') {
+                  idOfList.push(obj.id);
+                }
+              });
+
+              setTimeout(() => {
+                cardsInfo(boards, lists, cards, (cardData, id) => {
+                  idOfList.forEach((idOfList) => {
+                    if (idOfList === id) {
+                      console.log(cardData);
+                    }
+                  });
+                });
+              }, 2000);
+            }
+          });
+        }, 2000);
+      }
+    });
+  }, 2000);
+};
+
+module.exports = MindAndSpaceCards;
