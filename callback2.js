8f956a0d91e55be3421d288e7caacda094e75db7
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 14:02:25 +0530

    removed all commentout code like question and unwanted code.

--- callback2.js
+++ callback2.js
@@ -1,7 +1,3 @@
-/*
-	Problem 2: Write a function that will return all lists that belong to a board based on the boardID that is passed to it from the given data in lists.json. Then pass control back to the code that called it by using a callback function.
-*/
-
 const listInfo = (board, lists, cb) => {
   if (board.length === 0 || Object.keys(lists).length === 0) {
     throw new Error('Board or lists is Empty..');
