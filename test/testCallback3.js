ad14954dd941f505b843802be8b6a3997bfd9e28
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 19:59:56 +0530

    import all promise and consuming by then method and using Promise.all get info of cards simultaneously.

--- test/testCallback3.js
+++ test/testCallback3.js
@@ -1,13 +1,22 @@
-const cardsInfo = require("../callback3");
-const boards = require("../data/boards.json");
-const lists = require("../data/lists.json");
-const cards = require("../data/cards.json");
+const { boardId, listOfId, cardsInfo } = require('../callback3');
+const boards = require('../data/boards.json');
+const lists = require('../data/lists.json');
+const cards = require('../data/cards.json');
 
-try {
-  const result = cardsInfo(boards, lists, cards, (data, id) => {
-    console.log(id);
-    console.log(data);
+boardId(boards)
+  .then((idOfBoard) => {
+    return listOfId(lists, idOfBoard[0]);
+  })
+  .then((idOflist) => {
+    let singlePromise = [];
+    idOflist.forEach((id) => {
+      singlePromise.push(cardsInfo(cards, id));
+    });
+    return Promise.all(singlePromise);
+  })
+  .then((cardsData) => {
+    cardsData.forEach((cardInfo) => console.log(cardInfo));
+  })
+  .catch((error) => {
+    console.log(error.message);
   });
-} catch (error) {
-  console.log(error.message);
-}
