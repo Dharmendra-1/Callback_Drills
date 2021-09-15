7468a24f5ffe3f0beb1ca4ba1e4854f18f9f4c11
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 07:41:46 +0530

    import allCardsOfList , board, lists, cards and call allCardsOfList function with three argument.

---
+++ test/testCallback6.js
@@ -0,0 +1,10 @@
+const allCardsOfList = require('../callback6');
+const boards = require('../data/boards.json');
+const lists = require('../data/lists.json');
+const cards = require('../data/cards.json');
+
+try {
+  allCardsOfList(boards, lists, cards);
+} catch (error) {
+  console.log(error.message);
+}
