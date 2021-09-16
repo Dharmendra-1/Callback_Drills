bc11b3ffd37c17781d4c451de8815bdcc2a283f4
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 11:34:55 +0530

    create BoardInfo function declare as async and perform operation by async await and prints board data.

--- test/testCallback1.js
+++ test/testCallback1.js
@@ -1,18 +1,20 @@
 const { boardsInfo, boardId } = require('../callback1');
 const boards = require('../data/boards.json');
 
-boardId(boards)
-  .then((idOfBoard) => {
+const BoardInfo = async () => {
+  try {
+    let idOfBoard = await boardId(boards);
     let allPromise = [];
     idOfBoard.forEach((id) => {
       allPromise.push(boardsInfo(boards, id));
     });
 
-    return Promise.all(allPromise);
-  })
-  .then((infoOfBoard) => {
-    infoOfBoard.forEach((info) => console.log(info));
-  })
-  .catch((error) => {
+    allPromise.forEach(async (promise) => {
+      console.log(await promise);
+    });
+  } catch (error) {
     console.log(error.message);
-  });
+  }
+};
+
+BoardInfo();
