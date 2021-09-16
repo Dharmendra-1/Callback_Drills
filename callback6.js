8f7b16ed1ac651c863d6331d4665878c16ca05b4
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 08:39:15 +0530

    create infoOfAllCards function which return a promise and resolve id of list , import previous promise and consuming inside.

--- callback6.js
+++ callback6.js
@@ -1,46 +1,48 @@
-const boardInfo = require('./callback1');
-const listInfo = require('./callback2');
-const cardsInfo = require('./callback3');
+const { listInfo } = require('./callback2');
+const { boardId, boardsInfo } = require('./callback1');
 
-const allCardsOfList = (boards, lists, cards) => {
-  if (
-    board.length === 0 ||
-    Object.keys(lists).length === 0 ||
-    Object.keys(cards).length === 0
-  ) {
-    throw new Error('Boards or lists or cards is Empty..');
-  }
-
-  setTimeout(() => {
-    boardInfo(boards, (data) => {
-      if (data.name === 'Thanos') {
-        console.log(data);
-        let thanosId = data.id;
-
-        setTimeout(() => {
-          listInfo(boards, lists, (listData, id) => {
-            if (thanosId == id) {
-              console.log(listData);
-              let idOfList = [];
-              listData.forEach((obj) => {
-                idOfList.push(obj.id);
-              });
-
-              setTimeout(() => {
-                cardsInfo(boards, lists, cards, (cardData, id) => {
-                  idOfList.forEach((idOfList) => {
-                    if (idOfList === id) {
-                      console.log(cardData);
-                    }
-                  });
-                });
-              }, 2000);
-            }
-          });
-        }, 2000);
+const infoOfAllCards = (boards, lists, cards) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (
+        boards.length === 0 ||
+        Object.keys(lists).length === 0 ||
+        Object.keys(cards).length === 0
+      ) {
+        reject('Board or lists or card is empty');
       }
-    });
-  }, 2000);
+
+      boardId(boards)
+        .then((idOfBoard) => {
+          boardsInfo(boards, idOfBoard[0])
+            .then((info) => {
+              if (info.name === 'Thanos') {
+                console.log(info);
+
+                listInfo(lists, info.id)
+                  .then((infoOfList) => {
+                    console.log(infoOfList);
+
+                    let idOfAllList = [];
+                    infoOfList.forEach((obj) => {
+                      idOfAllList.push(obj.id);
+                    });
+                    resolve(idOfAllList);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 2000);
+  });
 };
 
-module.exports = allCardsOfList;
+module.exports = infoOfAllCards;
