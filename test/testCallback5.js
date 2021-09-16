e440cadcebdb1a5ff72831b9448b5b81f123c5f1
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 08:26:58 +0530

    import previous promise and resolve inside infoOfMindAndList function which return new promise and resolve listId.

--- test/testCallback5.js
+++ test/testCallback5.js
@@ -1,10 +1,22 @@
-const MindAndSpaceCards = require('../callback5');
+const infoOfMindAndList = require('../callback5');
+const { cardsInfo } = require('./callback3');
 const boards = require('../data/boards.json');
 const lists = require('../data/lists.json');
 const cards = require('../data/cards.json');
 
-try {
-  MindAndSpaceCards(boards, lists, cards);
-} catch (error) {
-  console.log(error.message);
-}
+infoOfMindAndList(boards, lists, cards)
+  .then((idOfList) => {
+    let SinglePromise = [];
+
+    idOfList.forEach((id) => {
+      SinglePromise.push(cardsInfo(cards, id));
+    });
+
+    return Promise.all(SinglePromise);
+  })
+  .then((card) => {
+    card.forEach((info) => console.log(info));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
