336336d4fa819738042821cec2bdb4e78434f3b6
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 08:40:30 +0530

    import infoOfAllCards, cardsInfo promise consuming both promise and get cards data.

--- test/testCallback6.js
+++ test/testCallback6.js
@@ -1,11 +1,22 @@
-const allCardsOfList = require('../callback6');
-
+const infoOfAllCards = require('../callback6');
 const boards = require('../data/boards.json');
 const lists = require('../data/lists.json');
 const cards = require('../data/cards.json');
+const { cardsInfo } = require('../callback3');
 
-try {
-  allCardsOfList(boards, lists, cards);
-} catch (error) {
-  console.log(error.message);
-}
+infoOfAllCards(boards, lists, cards)
+  .then((idOfList) => {
+    const SinglePromise = [];
+
+    idOfList.forEach((id) => {
+      SinglePromise.push(cardsInfo(cards, id));
+    });
+
+    return Promise.all(SinglePromise);
+  })
+  .then((allCard) => {
+    allCard.forEach((card) => console.log(card));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
